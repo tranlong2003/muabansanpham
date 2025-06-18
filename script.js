e53e23c36dd9596c1a8101285d250223355add3f
tranlong2003 <139291042+tranlong2003@users.noreply.github.com>
2025-06-18 16:58:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ function renderProducts(filterType) {
     div.innerHTML = `
       <div class="product-header">
   <h3>${p.name}</h3>
-  <button class="btn-view" onclick="openImageList(['${p.image}'])">📸</button>
+<button onclick="openImageList(['${p.image}'])">📸 Xem ảnh</button>
 </div>
       <p>Giá: ${p.price}</p>
       <p>${p.description}</p>
